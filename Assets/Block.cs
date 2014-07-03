bbe1d14a31f7d72e7b52c27da6464df65db3c0e7
movehoon <movehoon@gmail.com>
2014-07-04 02:15:46 +0900

    Add chuck from button

---
+++ Assets/Block.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+
+	void OnCollision() {
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/CreateChuck.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateChuck : MonoBehaviour {
+	public GameObject chuckPrefab;
+
+	public void OnClick()
+	{
+		Instantiate(chuckPrefab,new Vector3(transform.position.x, 1.0f, transform.position.z),Quaternion.identity);
+	}
+}
