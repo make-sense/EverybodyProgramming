e3b6a3eb2f892c3dc8930fedb7918ef6c9f3c79b
movehoon <movehoon@gmail.com>
2014-07-11 19:56:13 +0900

    Chuck attach and detach

--- Assets/Chuck.cs
+++ Assets/Chuck.cs
@@ -3,71 +3,136 @@ using System.Collections;
 
 public class Chuck : MonoBehaviour {
 
-	public UIRoot uiRoot = null;
-	private int CHUCK_WIDTH = 100;
-	private int CHUCK_HEIGHT = 75;
+	private float CHUCK_HEIGHT = 75f;
+	private float CHUCK_WIDTH = 100f;
+
+	private UIRoot _uiRoot = null;
+	private Chuck[] _children = new Chuck[2];
 
 	// Use this for initialization
 	void Start () {
+		if (UIRoot.list.Count > 0)
+			_uiRoot = UIRoot.list[0];
 		Debug.Log("Start");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		if (this.transform.parent != uiRoot)
-//			this.transform.localScale = new Vector3((float)CHUCK_WIDTH, 0f);
 	}
 
-	void OnCollisionEnter () {
-		Debug.Log ("OnCollisionEnter");
-	}
-
-	void OnCollisionExit() {
-		Debug.Log ("OnCollisionExit");
-		this.transform.parent = uiRoot.transform;
-	}
-
-	void OnTriggerEnter(Collider other) {
+	void OnTriggerEnter(Collider other) 
+	{
 		Debug.Log ("OnTriggerEnter");
 		if (other.tag == "Chuck") 
 		{
-			if (isRightEdge(this.transform, other.transform))
+			Vector3 srcPos = getGlobalPosition(this.transform);
+			Vector3 dstPos = getGlobalPosition(other.transform);
+			Debug.Log (srcPos.ToString() + "<=>" + dstPos.ToString());
+			if (isRightEdge(srcPos, dstPos))
 			{
 				this.transform.parent = other.transform;
-				this.transform.localPosition = new Vector3((float)CHUCK_WIDTH, 0f);
-
-				Debug.Log ("Set this to child");
+				this.transform.localPosition = new Vector3(CHUCK_WIDTH, 0);
+				Debug.Log ("Set this to right child");
+			}
+			else if (isBottomEdge(srcPos, dstPos))
+	        {
+				this.transform.parent = other.transform;
+				this.transform.localPosition = new Vector3(0, -CHUCK_HEIGHT);
+				Debug.Log ("Set this to bottom child");
 			}
 		}
 	}
 
-	void OnDragEnd () {
-		if (isChuckDisconnect(this.transform, this.transform.parent))
+	void OnDragEnd () 
+	{
+		if (isRoot(this.transform))
+		    return;
+
+		if (isChuckSeparated(this.transform))
 		{
 			Debug.Log ("isChuckDisconnect");
-			if (UIRoot.list.Count > 0)
-				this.transform.parent = UIRoot.list[0].transform;
+			this.transform.parent = _uiRoot.transform;
 		}
 		Debug.Log ("OnDragEnd");
 	}
 
-	void OnTriggerExit(Collider other) {
-		Debug.Log ("OnTriggerExit");
-		if (other.tag == "Chuck")
-		{
-			if (UIRoot.list.Count > 0)
-				this.transform.parent = UIRoot.list[0].transform;
-		}
-	}
+//	void OnTriggerExit(Collider other) 
+//	{
+//		Debug.Log ("OnTriggerExit");
+//		if (other.tag == "Chuck")
+//		{
+//			if (UIRoot.list.Count > 0)
+//				this.transform.parent = UIRoot.list[0].transform;
+//		}
+//	}
 
-	private bool isRightEdge(Transform src, Transform dst) {
-		if (src.localPosition.x > dst.localPosition.x + CHUCK_WIDTH/2)
+	private bool isRightEdge(Vector3 src, Vector3 dst) 
+	{
+		Debug.Log ("isRightEdge");
+		if (src.x > dst.x + CHUCK_WIDTH/2 &&
+		    src.x < dst.x + CHUCK_WIDTH &&
+		    src.y > dst.y - CHUCK_HEIGHT/2 &&
+		    src.y < dst.y + CHUCK_HEIGHT)
 			return true;
 		return false;
 	}
 
-	private bool isChuckDisconnect(Transform src, Transform dst) {
-		if (src.localPosition.x > dst.localPosition.x + CHUCK_WIDTH + 10)
+	private bool isBottomEdge(Vector3 src, Vector3 dst)
+	{
+		Debug.Log ("isBottomEdge");
+		if (src.x > dst.x &&
+		    src.x < dst.x + CHUCK_WIDTH/2 &&
+		    src.y > dst.y - CHUCK_HEIGHT &&
+		    src.y < dst.y - CHUCK_HEIGHT/2)
+			return true;
+		return false;
+	}
+
+	private bool isChuckSeparated(Transform src)
+	{
+		if (src.transform.localPosition.x < CHUCK_WIDTH -CHUCK_WIDTH*0.2f || 
+			src.transform.localPosition.x > CHUCK_WIDTH + CHUCK_WIDTH*0.2f ||
+		    src.transform.localPosition.y < -CHUCK_HEIGHT - CHUCK_HEIGHT*0.2f ||
+		    src.transform.localPosition.y > -CHUCK_HEIGHT + CHUCK_HEIGHT*0.2f)
+			return true;
+		return false;
+	}
+
+	private Vector3 getGlobalPosition(Transform transform)
+	{
+		if (isRoot (transform))
+			return transform.localPosition;
+		else
+		{
+			Vector3 pos = getGlobalPosition(transform.parent);
+			if (isRightChild(transform))
+				return new Vector3(pos.x+CHUCK_WIDTH, pos.y, 0);
+			else
+				return new Vector3(pos.x, pos.y-CHUCK_HEIGHT, 0);
+		}
+	}
+
+	private bool isRoot(Transform transform)
+	{
+		if (!transform.parent.name.Contains("Chuck"))
+			return true;
+		return false;
+	}
+
+	private bool isRightChild(Transform transform)
+	{
+		if (isRoot (transform))
+			return false;
+		if (transform.localPosition.x > CHUCK_WIDTH/2)
+			return true;
+		return false;
+	}
+
+	private bool isBottomChild(Transform transfor)
+	{
+		if (isRoot (transform))
+			return false;
+		if (transform.localPosition.y < -CHUCK_HEIGHT/2)
 			return true;
 		return false;
 	}
